afd3ed8fd292b9397351a15789ad42d947e1eb4d
tomitomi3 <tomi.nori+github@gmail.com>
2017-10-08 12:36:18 +0900

    1st commit

---
+++ NPolygon/Form1.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.oPlot = New OxyPlot.WindowsForms.PlotView()
+        Me.ctrlPanel = New System.Windows.Forms.Panel()
+        Me.tbxN = New System.Windows.Forms.TextBox()
+        Me.btnDraw = New System.Windows.Forms.Button()
+        Me.tbxResolution = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ctrlPanel.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'oPlot
+        '
+        Me.oPlot.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.oPlot.Location = New System.Drawing.Point(0, 0)
+        Me.oPlot.Name = "oPlot"
+        Me.oPlot.PanCursor = System.Windows.Forms.Cursors.Hand
+        Me.oPlot.Size = New System.Drawing.Size(684, 661)
+        Me.oPlot.TabIndex = 0
+        Me.oPlot.Text = "plot"
+        Me.oPlot.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE
+        Me.oPlot.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE
+        Me.oPlot.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS
+        '
+        'ctrlPanel
+        '
+        Me.ctrlPanel.Controls.Add(Me.Label1)
+        Me.ctrlPanel.Controls.Add(Me.tbxResolution)
+        Me.ctrlPanel.Controls.Add(Me.tbxN)
+        Me.ctrlPanel.Controls.Add(Me.btnDraw)
+        Me.ctrlPanel.Location = New System.Drawing.Point(12, 12)
+        Me.ctrlPanel.Name = "ctrlPanel"
+        Me.ctrlPanel.Size = New System.Drawing.Size(317, 29)
+        Me.ctrlPanel.TabIndex = 1
+        '
+        'tbxN
+        '
+        Me.tbxN.Location = New System.Drawing.Point(84, 5)
+        Me.tbxN.Name = "tbxN"
+        Me.tbxN.Size = New System.Drawing.Size(73, 19)
+        Me.tbxN.TabIndex = 1
+        Me.tbxN.Text = "3"
+        '
+        'btnDraw
+        '
+        Me.btnDraw.Location = New System.Drawing.Point(3, 3)
+        Me.btnDraw.Name = "btnDraw"
+        Me.btnDraw.Size = New System.Drawing.Size(75, 23)
+        Me.btnDraw.TabIndex = 0
+        Me.btnDraw.Text = "Draw"
+        Me.btnDraw.UseVisualStyleBackColor = True
+        '
+        'tbxResolution
+        '
+        Me.tbxResolution.Location = New System.Drawing.Point(242, 5)
+        Me.tbxResolution.Name = "tbxResolution"
+        Me.tbxResolution.Size = New System.Drawing.Size(70, 19)
+        Me.tbxResolution.TabIndex = 1
+        Me.tbxResolution.Text = "0.002"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(177, 8)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 12)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Resolution"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(684, 661)
+        Me.Controls.Add(Me.ctrlPanel)
+        Me.Controls.Add(Me.oPlot)
+        Me.Name = "Form1"
+        Me.Text = "Draw N Polygon"
+        Me.ctrlPanel.ResumeLayout(False)
+        Me.ctrlPanel.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents oPlot As OxyPlot.WindowsForms.PlotView
+    Friend WithEvents ctrlPanel As Panel
+    Friend WithEvents btnDraw As Button
+    Friend WithEvents tbxN As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents tbxResolution As TextBox
+End Class
---
+++ NPolygon/Form1.vb
@@ -0,0 +1,166 @@
+﻿Imports System.Threading.Tasks
+
+Public Class Form1
+    Dim isClick As Boolean = False
+    Dim previousPoint As Drawing.Point
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.oPlot.Model = New OxyPlot.PlotModel()
+        Me.oPlot.Model.PlotType = OxyPlot.PlotType.XY
+        Init(-3, 3, -3, 3)
+    End Sub
+
+    Public Sub Init(ByVal xMin As Double, ByVal xMax As Double, ByVal yMin As Double, ByVal yMax As Double)
+        Me.oPlot.Model.Series.Clear()
+        Me.oPlot.Model.Axes.Clear()
+
+        'plot setting
+        Me.oPlot.BackColor = Color.White
+        Me.oPlot.Model.PlotMargins = New OxyPlot.OxyThickness(0, 0, 0, 0)
+        Me.oPlot.Model.Padding = New OxyPlot.OxyThickness(0, 0, 0, 0)
+
+        'set view
+        Dim x = New OxyPlot.Axes.LinearAxis()
+        x.Position = OxyPlot.Axes.AxisPosition.Top
+        x.Minimum = xMin
+        x.Maximum = xMax
+        x.PositionAtZeroCrossing = True
+        x.AxislineStyle = OxyPlot.LineStyle.Automatic
+        x.AxislineColor = OxyPlot.OxyColors.Black
+        Me.oPlot.Model.Axes.Add(x)
+
+        Dim y = New OxyPlot.Axes.LinearAxis()
+        y.Position = OxyPlot.Axes.AxisPosition.Right
+        y.Minimum = yMin
+        y.Maximum = yMax
+        y.PositionAtZeroCrossing = True
+        y.AxislineStyle = OxyPlot.LineStyle.Automatic
+        y.AxislineColor = OxyPlot.OxyColors.Black
+        Me.oPlot.Model.Axes.Add(y)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnDraw.Click
+        Me.oPlot.Model.Series.Clear()
+
+        Try
+            Dim n = Double.Parse(tbxN.Text())
+            If n <= 2 Then
+                Return
+            End If
+
+            Dim allPoints = New OxyPlot.Series.ScatterSeries()
+            allPoints.MarkerType = OxyPlot.MarkerType.Circle
+            allPoints.MarkerSize = 1
+            allPoints.MarkerStroke = OxyPlot.OxyColors.Green
+
+            Dim resoulution = Double.Parse(tbxResolution.Text())
+
+            Dim stepWidth As Double = resoulution
+            Dim rangeMin As Double = -2.0 - (stepWidth * 2)
+            Dim rangeMax As Double = 2.0 + (stepWidth * 2)
+            Dim plotCondition As Double = stepWidth * 1.1
+
+            'single thread
+            'For x As Double = rangeMin To rangeMax Step stepWidth
+            '    For y As Double = rangeMin To rangeMax Step stepWidth
+            '        Dim temp = f(x, y, n)
+            '        If temp >= -plotCondition AndAlso temp <= plotCondition Then
+            '            allPoints.Points.Add(New OxyPlot.Series.ScatterPoint(x, y))
+            '        End If
+            '    Next
+            'Next
+
+            'multi thread
+            Dim allPoints2 = New OxyPlot.Series.ScatterSeries()
+            allPoints2.MarkerType = OxyPlot.MarkerType.Circle
+            allPoints2.MarkerSize = 1
+            allPoints2.MarkerStroke = OxyPlot.OxyColors.Green
+            Parallel.Invoke(Sub()
+                                For x As Double = rangeMin To 0 Step stepWidth
+                                    For y As Double = rangeMin To rangeMax Step stepWidth
+                                        Dim temp = f(x, y, n)
+                                        '0±plotConditionの範囲内であれば点をうつ
+                                        If temp >= -plotCondition AndAlso temp <= plotCondition Then
+                                            allPoints.Points.Add(New OxyPlot.Series.ScatterPoint(x, y))
+                                        End If
+                                    Next
+                                Next
+                            End Sub,
+                            Sub()
+                                For x As Double = 0 To rangeMax Step stepWidth
+                                    For y As Double = rangeMin To rangeMax Step stepWidth
+                                        Dim temp = f(x, y, n)
+                                        '0±plotConditionの範囲内であれば点をうつ
+                                        If temp >= -plotCondition AndAlso temp <= plotCondition Then
+                                            allPoints2.Points.Add(New OxyPlot.Series.ScatterPoint(x, y))
+                                        End If
+                                    Next
+                                Next
+                            End Sub)
+
+            Me.oPlot.Model.Series.Add(allPoints)
+            Me.oPlot.Model.Series.Add(allPoints2)
+            Me.oPlot.InvalidatePlot(True)
+        Catch ex As Exception
+            'do nothing
+        End Try
+    End Sub
+
+    Private Function fc(x As Double, y As Double) As Double
+        Return 1 - x * x - y * y
+    End Function
+
+    ''' <summary>
+    ''' 正n角形の式 MathPower2017
+    ''' </summary>
+    ''' <param name="x"></param>
+    ''' <param name="y"></param>
+    ''' <param name="n"></param>
+    ''' <returns></returns>
+    Private Function f(x As Double, y As Double, n As Double) As Double
+        '=0になるよう変形
+        Dim temp = p(x, y, n)
+        Dim val = x * x + y * y - 1.0 - temp * temp
+
+        If Double.IsNaN(val) = True Then
+            Return 1
+        End If
+        Return val
+    End Function
+
+    Private Function p(x As Double, y As Double, n As Double) As Double
+        Dim a1 = Math.Atan(x / y) - (Math.PI / 2.0) * sign(y)
+        Dim a3 = Math.Tan((n / 2.0) * a1)
+        Dim a4 = (2.0 / n) * Math.Atan(a3)
+        Dim a5 = Math.Tan(a4)
+        Return a5
+    End Function
+
+    Private Function sign(x As Double) As Double
+        If x < 0 Then
+            Return 1.0
+        ElseIf x > 0 Then
+            Return -1.0
+        Else
+            Return 0
+        End If
+    End Function
+
+    Private Sub Panel1_MouseDown(sender As Object, e As MouseEventArgs) Handles ctrlPanel.MouseDown
+        isClick = True
+        previousPoint = e.Location
+    End Sub
+
+    Private Sub Panel1_MouseMove(sender As Object, e As MouseEventArgs) Handles ctrlPanel.MouseMove
+        If isClick Then
+            Dim movePoint As Drawing.Point
+            movePoint.X = ctrlPanel.Location.X + e.X - previousPoint.X
+            movePoint.Y = ctrlPanel.Location.Y + e.Y - previousPoint.Y
+            ctrlPanel.Location = movePoint
+        End If
+    End Sub
+
+    Private Sub Panel1_MouseUp(sender As Object, e As MouseEventArgs) Handles ctrlPanel.MouseUp
+        isClick = False
+    End Sub
+End Class
